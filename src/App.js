5f8863726a89534dca86833022b848e57c27b530
Taylor Friesen <taylorfriesen@protonmail.com>
2021-03-18 19:41:18 -0700

    step 4 complete

--- src/App.js
+++ src/App.js
@@ -26,7 +26,7 @@ function App() {
 
 				{/* Routes */}
 				<Route exact path="/">
-					<Products products={products} addItem={addItem} />
+					<Products/>
 				</Route>
 
 				<Route path="/cart">
